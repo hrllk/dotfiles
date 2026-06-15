87a8df8df1ee8b5d8e340f7ee872dc1826848ecb
hrllk <heykia3@protonmail.com>
2026-06-15 14:12:24 +0900

    feat(zsh): make safe paste self-contained

--- zsh/plugins/safe-paste.zsh
+++ zsh/plugins/safe-paste.zsh
@@ -1,3 +1,58 @@
-if [[ -r "$HOME/.oh-my-zsh/plugins/safe-paste/safe-paste.plugin.zsh" ]]; then
-  source "$HOME/.oh-my-zsh/plugins/safe-paste/safe-paste.plugin.zsh"
+autoload -Uz is-at-least
+
+if ! is-at-least 5.1; then
+  return 0
 fi
+
+set zle_bracketed_paste
+
+bindkey -N bracketed-paste
+bindkey -RM bracketed-paste '\x00-\xFF' bracketed-paste-enqueue
+bindkey -M main '^[[200~' _bracketed_paste_begin
+bindkey -M bracketed-paste '^[[201~' _bracketed_paste_end
+bindkey -M bracketed-paste -s '^M' '^J'
+
+zle -N _bracketed_paste_begin
+zle -N _bracketed_paste_end
+zle -N bracketed-paste-enqueue _bracketed_paste_enqueue
+
+if typeset -f hooks-add-hook >/dev/null 2>&1; then
+  hooks-add-hook zle_line_init_hook _bracketed_paste_zle_init
+  hooks-add-hook zle_line_finish_hook _bracketed_paste_zle_finish
+else
+  zle -N zle-line-init _bracketed_paste_zle_init
+  zle -N zle-line-finish _bracketed_paste_zle_finish
+fi
+
+_bracketed_paste_begin() {
+  _bracketed_paste_restore_keymap=( ${(z)"$(bindkey -lL main)"} )
+  bindkey -A bracketed-paste main
+}
+
+_bracketed_paste_end() {
+  local content="${_bracketed_paste_content//$'\r'/ }"
+  content="${content//$'\n'/ }"
+
+  if [ ${_bracketed_paste_restore_keymap[@]:0:1} = 'bindkey' ]; then
+    $_bracketed_paste_restore_keymap
+  fi
+  LBUFFER+="$content"
+  unset _bracketed_paste_content _bracketed_paste_restore_keymap
+}
+
+_bracketed_paste_enqueue() {
+  _bracketed_paste_content+=$KEYS
+}
+
+_bracketed_paste_zle_init() {
+  _bracketed_paste_content=''
+  if [[ $TERM =~ '^(rxvt-unicode|xterm(-256color)?|screen(-256color)?)$' ]]; then
+    printf '\e[?2004h'
+  fi
+}
+
+_bracketed_paste_zle_finish() {
+  if [[ $TERM =~ '^(rxvt-unicode|xterm(-256color)?|screen(-256color)?)$' ]]; then
+    printf '\e[?2004l'
+  fi
+}
