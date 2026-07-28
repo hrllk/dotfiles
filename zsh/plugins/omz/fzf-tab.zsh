f6e73b6fdb65f3e3968c6829467229a3e25d92dc
hrllk <heykia3@protonmail.com>
2026-07-28 23:25:39 +0900

    fix: make fzf-tab completion optional

--- zsh/plugins/omz/fzf-tab.zsh
+++ zsh/plugins/omz/fzf-tab.zsh
@@ -1,15 +1,23 @@
-if [[ -r "$ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh" ]]; then
-  source "$ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh"
+fzf_tab_plugin="$ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh"
+
+# fzf-tab needs the fzf executable at runtime. Keep normal zsh completion
+# available when the optional dependency is not installed yet.
+if (( $+commands[fzf] )) && [[ -r "$fzf_tab_plugin" ]]; then
+  source "$fzf_tab_plugin"
+
+  # Let fzf-tab own the completion menu instead of zsh's select menu.
+  zstyle ':completion:*' menu no
+  zstyle ':fzf-tab:*' continuous-trigger '/'
+  zstyle ':fzf-tab:*' switch-group '<' '>'
+  zstyle ':fzf-tab:complete:cd:*' fzf-preview '
+    if command -v colorls >/dev/null 2>&1; then
+      colorls -1 --gs --sd "$realpath"
+    elif command -v eza >/dev/null 2>&1; then
+      eza -1 --color=always "$realpath"
+    else
+      ls -laG "$realpath"
+    fi
+  '
 fi
 
-zstyle ':fzf-tab:*' continuous-trigger '/'
-zstyle ':fzf-tab:*' switch-group '<' '>'
-zstyle ':fzf-tab:complete:cd:*' fzf-preview '
-  if command -v colorls >/dev/null 2>&1; then
-    colorls -1 --gs --sd "$realpath"
-  elif command -v eza >/dev/null 2>&1; then
-    eza -1 --color=always "$realpath"
-  else
-    ls -laG "$realpath"
-  fi
-'
+unset fzf_tab_plugin
